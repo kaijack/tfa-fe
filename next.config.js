4cdca26fcf239163cbddc2d0b91f40026d2590ad
zak <ahmad.zaki@mst.co.id>
2025-02-20 22:38:46 +0700

    out

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@styles': path.resolve(__dirname, 'styles'),
-    };
-    return config;
-  },
-  output: 'export', 
+  output: "export", // Export static files
+  distDir: "out",   // Match the `bucket` directory in `wrangler.toml`
 };
