fd8a9622f15a451178f2404c6b4e9d57813cd5ff
zak <ahmad.zaki@mst.co.id>
2025-03-11 02:24:14 +0700

    children

--- components/MenuTree/MenuTreeNode.tsx
+++ components/MenuTree/MenuTreeNode.tsx
@@ -37,12 +37,14 @@ const MenuTreeNode: React.FC<MenuTreeNodeProps> = ({
     <li className="relative">
       <div className="absolute top-0 left-0 w-px h-full bg-gray-300"></div>
       <div className="flex items-center relative pl-4">
-        <div className="absolute left-0 top-1/2 w-4 h-px bg-gray-300"></div>
+        {
+          menu?.children?.length === 0 ? <div className="absolute left-0 top-1/2 w-9 h-px bg-gray-300"></div> : <div className="absolute left-0 top-1/2 w-4 h-px bg-gray-300"></div>
+        }
 
         {/* Expand/Collapse Button */}
         {(menu.children?.length > 0 || menu.depth < maxDepth && menu.depth === 0 && maxDepth === 0) && (
           <button
-            className="p-1 text-gray-500 hover:text-gray-800"
+            className="p-1 text-gray-500 hover:text-gray-800 pl-1"
             onClick={() => toggleExpand(menu.id)}
           >
             {isExpanded ? <AiOutlineDown size={16} /> : <AiOutlineRight size={16} />}
@@ -50,27 +52,37 @@ const MenuTreeNode: React.FC<MenuTreeNodeProps> = ({
         )}
 
         {/* Menu Name */}
-        <span
-          className="cursor-pointer hover:text-blue-500"
-          onClick={() => {
-            setSelectedMenu(menu);
-            setShowForm(true);
-          }}
-        >
-          {menu.name}
-        </span>
+        {
+          menu?.children?.length === 0 ?
 
+            <span
+              className="cursor-pointer hover:text-blue-500 pl-7"
+              onClick={() => {
+                setSelectedMenu(menu);
+                setShowForm(true);
+              }}
+            >
+              {menu.name}
+            </span> :
+
+            <span
+              className="cursor-pointer hover:text-blue-500 pl-1"
+              onClick={() => {
+                setSelectedMenu(menu);
+                setShowForm(true);
+              }}
+            >
+              {menu.name}
+            </span>
+
+        }
         {/* Add Child Button */}
-        {/* {(Number(menu?.children?.length) === (maxDepth - menu?.children?.length) || menu.depth < maxDepth && menu.depth === 0 && maxDepth === 0) && ( */}
-        { menu.canAddChild && (
+        {menu.canAddChild && (
           <button
             className="ml-2 p-1 text-blue-500 hover:text-blue-700"
             onClick={() => addChildMenu(menu)}
           >
             <FiPlus size={16} />
-            {/* {(menu?.children?.length)} <br />
-            {maxDepth} <br />
-            {menu.depth} */}
           </button>
         )}
       </div>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,12 +11,11 @@ const Header: React.FC<HeaderProps> = ({ title, isSidebarOpen, setIsSidebarOpen 
   return (
     <div className="flex flex-col items-start pl-4">
       {/* Button */}
-      <div className="flex items-center space-x-2 text-sm mt-3">
+      <div className="flex items-center space-x-2 text-sm mt-3 -ml-1">
         {!isSidebarOpen && (
           <button
-            className={`top-5 transition-all duration-300 z-50 rounded-md text-black ${
-              isSidebarOpen ? "left-[100px]" : "left-4"
-            }`}
+            className={`top-5 transition-all duration-300 z-50 rounded-md text-black ${isSidebarOpen ? "left-[100px]" : "left-4"
+              }`}
             onClick={() => setIsSidebarOpen(!isSidebarOpen)}
           >
             <div
@@ -34,8 +33,9 @@ const Header: React.FC<HeaderProps> = ({ title, isSidebarOpen, setIsSidebarOpen 
 
       {/* Folder Icon and Title */}
       <span className="flex items-center mt-2">
-        <FaFolder className="text-gray-400 mr-2" />
-        <span className="ml-3">/ {title}</span>
+        <FaFolder className="text-gray-300 mr-2" />
+        <span className="ml-1 text-gray-300"> / </span>
+        <span className="ml-2"> {title}</span>
       </span>
     </div>
   );
